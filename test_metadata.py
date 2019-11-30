87480d6259b37e0f0de597f951b0e6ef0911447d
Brian Rutledge <brian@bhrutledge.com>
2019-11-29 20:25:46 -0500

    Fix 'name already defined' error

--- test_metadata.py
+++ test_metadata.py
@@ -1,7 +1,7 @@
 try:
     import importlib.metadata as importlib_metadata
 except ImportError:
-    import importlib_metadata
+    import importlib_metadata  # type: ignore  # https://github.com/python/mypy/issues/1153
 
 metadata = importlib_metadata.metadata('mypy')
 print(metadata['version'])
