b5bd621d4239dd8be3495a052d3ca9d43ee7a213
Brian Rutledge <brian@bhrutledge.com>
2019-11-29 20:34:52 -0500

    Fix 'Module has no attribute' error

--- test_metadata.py
+++ test_metadata.py
@@ -3,5 +3,5 @@ try:
 except ImportError:
     import importlib_metadata  # type: ignore  # https://github.com/python/mypy/issues/1153
 
-metadata = importlib_metadata.metadata('mypy')
+metadata = importlib_metadata.metadata('mypy')  # type: ignore  # https://github.com/python/mypy/issues/1393
 print(metadata['version'])
