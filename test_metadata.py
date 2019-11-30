125b08ccc4dc56de927ac750f7feaad2224dfd07
Brian Rutledge <brian@bhrutledge.com>
2019-11-29 20:40:09 -0500

    Improve readability

--- test_metadata.py
+++ test_metadata.py
@@ -1,7 +1,7 @@
 try:
-    import importlib.metadata as importlib_metadata
+    from importlib.metadata import metadata  # type: ignore  # https://github.com/python/mypy/issues/1393
 except ImportError:
-    import importlib_metadata  # type: ignore  # https://github.com/python/mypy/issues/1153
+    from importlib_metadata import metadata  # type: ignore  # https://github.com/python/mypy/issues/1153
 
-metadata = importlib_metadata.metadata('mypy')  # type: ignore  # https://github.com/python/mypy/issues/1393
-print(metadata['version'])
+meta = metadata('mypy')
+print(meta['version'])
