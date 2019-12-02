be26bd0f6cf41fe5982afe51358ec31948116aa6
Brian Rutledge <brian@bhrutledge.com>
2019-12-01 21:21:06 -0500

    Rename variable

--- test_metadata.py
+++ test_metadata.py
@@ -3,5 +3,5 @@ try:
 except ImportError:
     from importlib_metadata import metadata  # type: ignore  # https://github.com/python/mypy/issues/1153
 
-meta = metadata('mypy')
-print(meta['version'])
+mypy_metadata = metadata('mypy')
+print(mypy_metadata['version'])
